ceb1dfa32942722fa705c0625d2fc2fc186129e8
remo5000 <vigneshshankar@gmail.com>
2018-05-17 23:22:12 +0800

    Add bot helper functions

--- init.py
+++ init.py
@@ -1,8 +1,9 @@
 import telegram
 from credentials import *
 
-# For testing
-bot = telegram.Bot(token=get_api_key())
-bot.send_message(chat_id=get_user_id(), text='YEEHEEE boizzz!')
+def get_bot():
+    bot = telegram.Bot(token=get_api_key())
+    return bot
 
-
+def test_bot():
+    get_bot().send_message(chat_id=get_user_id(), text='YEEHEEE boizzz!')
