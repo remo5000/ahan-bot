aa4990c5f3216c65fdb44df04ab5fa6f5a64b24f
remo5000 <vigneshshankar@gmail.com>
2018-05-18 00:00:19 +0800

    Remove echo

--- messageHandler.py
+++ messageHandler.py
@@ -3,10 +3,6 @@ from init import *
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
 
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-    # text = update.message.text here because we want to retrieve the text from the original message and send the same thing back
-
 tech_buzzwords = ["ar", "vr", "blockchain", "crypto", \
         "machine learning", "cloud", "HD"]
 
