8797bd5e41a5d4a9b9454708ae54471528e9be81
circularhorses <jefferykwoh@gmail.com>
2018-05-20 12:17:31 +0800

    added mala, made regex more lenient

--- messageHandler.py
+++ messageHandler.py
@@ -10,7 +10,7 @@ from telegram.ext import MessageHandler, Filters
 
 # Hardcoding triggers and responses.
 # Tech related
-tech_buzzwords = re.compile(r"\bar\b|\bvr\b|\bblockchain\b|\bcrypto\b|\bmachine learning\b|\bcloud\b|\bHD\b",
+tech_buzzwords = re.compile(r"\bar\b|\bvr\b|\bblock[\s]*chain[s]*\b|\bcrypto\b|\bmachine[\s]*learning\b|\bcloud\b|\bHD\b",
                      flags=re.IGNORECASE | re.MULTILINE)
 tech_buzzwords_reply = ["AR VR BLOCKCHAIN CRYTO CLOUD MACHINE LEARNING HHHHHHHHHD PORRRRNNNNNN!!!!!"]
 
@@ -20,7 +20,7 @@ triggers = re.compile(r"\bstfu\b.*\bahan\b|\bfuck\b.*\bahan\b",
 trigger_replies = ["dohohohoooont be a fucker lah OUI", "LULZ", "fk youuuu LULZ jk luv <3", "thx bb xoxo", "BODOH LA U"]
 
 # Food / Ameens
-food_words= re.compile(r"\bsupper\b|\bameens\b|\bmacs\b|\bcheese fries\b|\bordering\b",
+food_words= re.compile(r"\bsupper\b|\ba+m+e+n+s*\b|\bmacs+\b|\bcheese[\s]*fries+\b|\border(ing)?\b|\bmala+\b",
                      flags=re.IGNORECASE | re.MULTILINE)
 food_replies = ["so ameens???", "you noe what time it is right", "save some cheese fries for me plez <333",
                 "CHEEEESE FRIES", "now we HAVE to order", "fuck that, anyone wnna eat some MALA???"]
@@ -52,3 +52,4 @@ dispatcher = updater.dispatcher
 echo_handler = MessageHandler((Filters.text), chainwax)
 dispatcher.add_handler(echo_handler)
 updater.start_polling()
+print("Ahan bot started\nYEHEEHEEHEE") # Init message.
